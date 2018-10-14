b01f8bbff8fa9bd5f4559a7674b16ac818058fcf
twinemarron <twinemarron@gmail.com>
2018-10-14 21:51:38 +0900

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+const env = process.env.NODE_ENV || 'development'
+const isDevelopment = env === 'development'
+
+module.exports = {
+  mode: env,
+  target: 'node',
+  devtool: isDevelopment ? 'source-map' : false,
+  output: {
+    path: __dirname,
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  externals: [nodeExternals()],
+}
+
