a10d51b326c5addae33b08233d1c89b37cf0c41b
twinemarron <twinemarron@gmail.com>
2019-01-19 07:00:49 +0900

    Add prettier

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -13,9 +13,10 @@ export type AppSetting = {
 
 const getOpenDBRequest = (): IDBOpenDBRequest | null => {
   try {
-    const request = (typeof window !== 'undefined' && window && window.indexedDB)
-      ? window.indexedDB.open(DB_NAME, VERSION)
-      : null
+    const request =
+      typeof window !== 'undefined' && window && window.indexedDB
+        ? window.indexedDB.open(DB_NAME, VERSION)
+        : null
     return request
   } catch (err) {
     return null
@@ -25,26 +26,26 @@ const getOpenDBRequest = (): IDBOpenDBRequest | null => {
 export const putData = (data: AppSetting): void => {
   let db, objectStore
   const openDBRequest = getOpenDBRequest()
-  if(!openDBRequest) return
-  openDBRequest.onsuccess = (event) => {
+  if (!openDBRequest) return
+  openDBRequest.onsuccess = event => {
     console.log('event!!!!')
     console.log(event)
-    if(!event || !event.target) return
+    if (!event || !event.target) return
     console.log('onsuccess event: ', event)
     const db = (event.target as IDBOpenDBRequest).result
     const transaction = db.transaction(STORE_NAME, 'readwrite')
     const objectStore = transaction.objectStore(STORE_NAME)
     const request = objectStore.put(data)
     request.onsuccess = () => {
-      console.log('データの挿入に成功') 
+      console.log('データの挿入に成功')
     }
     transaction.oncomplete = () => {
-      console.log('トランザクション完了') 
+      console.log('トランザクション完了')
     }
   }
   openDBRequest.onupgradeneeded = (event): void => {
     console.log('onupgradeneeded event: ', event)
-    if(!event || !event.target) return
+    if (!event || !event.target) return
     const db = (event.target as IDBOpenDBRequest).result
     const objectStore = db.createObjectStore(STORE_NAME, { keyPath: KEY_PATH })
   }
@@ -58,27 +59,27 @@ export const getData = (key: string): Promise<AppSetting> => {
   let db, objectStore
   const openDBRequest = getOpenDBRequest()
   return new Promise((resolve, reject) => {
-    if(!openDBRequest) {
+    if (!openDBRequest) {
       reject()
       return
     }
     openDBRequest.onsuccess = (event): void => {
-      if(!event || !event.target) return
+      if (!event || !event.target) return
       console.log('onsuccess event: ', event)
       const db = (event.target as IDBOpenDBRequest).result
       const transaction = db.transaction([STORE_NAME], 'readonly')
       const objectStore = transaction.objectStore(STORE_NAME)
       const request = objectStore.get(key)
-      request.onsuccess = (event) => {
-        console.log('データの取得に成功 event: ', event) 
-        console.log('データの取得に成功 request: ', request) 
+      request.onsuccess = event => {
+        console.log('データの取得に成功 event: ', event)
+        console.log('データの取得に成功 request: ', request)
         resolve(request.result)
       }
       transaction.oncomplete = () => {
-        console.log('トランザクション完了') 
+        console.log('トランザクション完了')
       }
     }
-    openDBRequest.onerror = (event) => {
+    openDBRequest.onerror = event => {
       console.log('onerror event: ', event)
       reject()
     }
